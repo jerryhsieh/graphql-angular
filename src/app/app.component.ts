d387d792e69d34893711df04e90dfb191ae5f90f
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-07-02 15:29:30 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+
 import { Component } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { map, scan } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
   title = 'app';
+  $users: Observable<User[]>;
+  constructor(apollo: Apollo) {
+    this.$users = apollo.query<any>({
+      query: gql`
+            {
+              users {
+                      id
+                      username
+                      email
+              }
+            }
+`})
+      .pipe(
+        map(res => res.data.users),
+        scan(users => console.log)
+      )
+
+  }
+}
+
+interface User {
+  id: String,
+  username: String,
+  email: String
 }
